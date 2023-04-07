5254d9a6f1ffc9024365e85e34cb9e1d5a49bc90
Nikita Ozhegov <nikita.ozhegov8@mail.ru>
2023-04-07 09:45:56 +0400

    limit room

---
+++ src/pages/RoomPage/ui/RoomIsFull/RoomIsFull.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material"
+import styles from "./RoomIsFull.module.scss"
+
+type RoomIsFullProps = {}
+
+export const RoomIsFull = (props: RoomIsFullProps) => {
+  const {} = props
+  return <Typography variant="h1">Room is full</Typography>
+}
