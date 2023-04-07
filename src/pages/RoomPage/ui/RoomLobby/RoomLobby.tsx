8d2cd79b80f7356fff64054206df1faa44a8891e
Nikita Ozhegov <nikita.ozhegov8@mail.ru>
2023-04-07 11:12:55 +0400

    fix room

--- src/pages/RoomPage/ui/RoomLobby/RoomLobby.tsx
+++ src/pages/RoomPage/ui/RoomLobby/RoomLobby.tsx
@@ -1,13 +1,30 @@
+import { socketClient } from "@/shared/api/socket/socket"
 import { Chat } from "@/widgets/Chat"
 import { Stack } from "@mui/material"
+import { useEffect, useState } from "react"
 import { useWebRTCRoom } from "../../lib/useWebRTCRoom/useWebRTCRoom"
 import { RoomActions } from "../RoomActions/RoomActions"
+import { RoomIsFull } from "../RoomIsFull/RoomIsFull"
 import { RoomStreams } from "../RoomStreams/RoomStreams"
 import { RoomUsers } from "../RoomUsers/RoomUsers"
 import styles from "./RoomLobby.module.scss"
 
 export const RoomLobby = () => {
   const { handleSendMessage, handleSendBlob } = useWebRTCRoom()
+  const [roomisFull, setRoomIsfull] = useState(false)
+
+  useEffect(() => {
+    socketClient.on("room_is_full", () => {
+      setRoomIsfull(true)
+    })
+    return () => {
+      socketClient.off("room_is_full", () => {})
+    }
+  }, [])
+
+  if (roomisFull) {
+    return <RoomIsFull />
+  }
 
   return (
     <Stack justifyContent="space-between" direction="row" height="100%">
--- src/pages/RoomPage/ui/RoomPage/RoomPage.tsx
+++ src/pages/RoomPage/ui/RoomPage/RoomPage.tsx
@@ -1,11 +1,10 @@
 import { PageWrapper } from "@/widgets/Page"
 import { useParams } from "react-router-dom"
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { socketClient } from "@/shared/api/socket/socket"
 import { RoomLobby } from "../RoomLobby/RoomLobby"
 import { useUserStore } from "@/entities/User"
 import { useRoomRTCStore } from "../../model/store/RoomRTCStore"
-import { RoomIsFull } from "../RoomIsFull/RoomIsFull"
 
 const emitToJoinRoom = (id: string) => {
   const username = useUserStore.getState().localUser.username
@@ -24,16 +23,12 @@ export const RoomPage = () => {
   const { id = "" } = useParams()
   const joinToRoom = useRoomRTCStore((state) => state.joinRoom)
   const leaveRoom = useRoomRTCStore((state) => state.leaveRoom)
-  const [roomisFull, setRoomIsfull] = useState(false)
 
   useEffect(() => {
     joinToRoom(id)
     const fn = emitToJoinRoom(id)
     const prevName = document.title
     document.title = `${document.title} - ${id}`
-    socketClient.on("room_is_full", () => {
-      setRoomIsfull(true)
-    })
 
     return () => {
       document.title = prevName
@@ -42,14 +37,6 @@ export const RoomPage = () => {
       fn()
     }
   }, [id, joinToRoom, leaveRoom])
-
-  if (roomisFull) {
-    return (
-      <PageWrapper>
-        <RoomIsFull />
-      </PageWrapper>
-    )
-  }
 
   return (
     <PageWrapper>
