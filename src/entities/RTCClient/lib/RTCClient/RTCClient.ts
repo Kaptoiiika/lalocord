e292b99303eb49129aaa15a647b6cadbf97b93b2
Nikita Ozhegov <nikita.ozhegov8@mail.ru>
2023-05-04 21:47:13 +0400

    test codec

--- src/entities/RTCClient/lib/RTCClient/RTCClient.ts
+++ src/entities/RTCClient/lib/RTCClient/RTCClient.ts
@@ -146,17 +146,20 @@ export class RTCClient extends Emitter<RTCClientEvents> {
 
   async createOffer() {
     if (!this.peer) return
-    const tcvr = this.peer.getTransceivers()
-    const codecs = RTCRtpReceiver.getCapabilities("video")?.codecs
-    if (codecs) {
-      const h264 = codecs?.filter((codecs) => {
-        return codecs.mimeType === "video/H264"
-      })
-      console.log("selected", tcvr, h264)
-      tcvr.forEach((tcv) => {
-        if (tcv.sender.track?.kind === "video") tcv.setCodecPreferences?.(h264)
-      })
-    }
+    try {
+      const tcvr = this.peer.getTransceivers()
+      const codecs = RTCRtpReceiver.getCapabilities("video")?.codecs
+      if (codecs) {
+        const h264 = codecs?.filter((codecs) => {
+          return codecs.mimeType === "video/H264"
+        })
+        console.log("selected", tcvr, h264)
+        tcvr.forEach((tcv) => {
+          if (tcv.sender.track?.kind === "video")
+            tcv.setCodecPreferences?.(h264)
+        })
+      }
+    } catch (error) {}
     const offer = await this.peer.createOffer()
     await this.peer.setLocalDescription(offer)
     const data = { id: this.id, offer: offer }
