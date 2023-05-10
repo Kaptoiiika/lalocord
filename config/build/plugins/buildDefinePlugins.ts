50933cd1247313e822c31b198d91dfa0a6009dd9
Nikita Ozhegov <nikita.ozhegov8@mail.ru>
2023-05-10 19:26:00 +0400

    add buildV

--- config/build/plugins/buildDefinePlugins.ts
+++ config/build/plugins/buildDefinePlugins.ts
@@ -9,7 +9,7 @@ export const buildDefinePlugins = (options: {
   return new webpack.DefinePlugin({
     __IS_DEV__: JSON.stringify(isDev),
     __API_URL__: JSON.stringify(apiURL),
-    __BUILD_VERSION__: JSON.stringify(`${new Date().getDate()}-${new Date().getMonth()}-${new Date().getFullYear()}`),
+    __BUILD_VERSION__: JSON.stringify(`${new Date().getDate()}-${new Date().getMonth()+1}-${new Date().getFullYear()}`),
   })
 }
 
--- src/widgets/Footer/ui/AppFooter.tsx
+++ src/widgets/Footer/ui/AppFooter.tsx
@@ -8,12 +8,18 @@ export const AppFooter = (props: AppFooterProps) => {
   const {} = props
   return (
     <footer className={styles.footer}>
-      <Typography variant="h6">Contact</Typography>
-      <ul>
-        <MUILink href="mailto:nikita.ozhegov8@mail.ru">
-          nikita.ozhegov8@mail.ru
-        </MUILink>
-      </ul>
+      <div>
+        <Typography variant="h6">Contact</Typography>
+        <ul>
+          <MUILink href="mailto:nikita.ozhegov8@mail.ru">
+            nikita.ozhegov8@mail.ru
+          </MUILink>
+        </ul>
+      </div>
+      <div>
+        <Typography variant="h6">Build version</Typography>
+        <Typography align="right">{__BUILD_VERSION__}</Typography>
+      </div>
     </footer>
   )
 }
